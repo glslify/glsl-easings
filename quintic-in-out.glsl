a83e5f7d99c8a067e13679894d1d05f410e00d3f
Ben Schmidt <bmschmidt@gmail.com>
2020-07-17 16:14:24 -0400

    fix functions, add debug code

--- quintic-in-out.glsl
+++ quintic-in-out.glsl
@@ -1,7 +1,9 @@
-float qinticInOut(float t) {
-  return t < 0.5
-    ? +16.0 * pow(t, 5.0)
-    : -0.5 * pow(2.0 * t - 2.0, 5.0) + 1.0;
+float quinticInOut(float t) {
+  if (t < 0.5) {
+    return 16.0 * t * t * t * t * t;
+  }
+  float u = 1.0 - t;
+  return 1.0 - 16.0 * u * u * u * u * u;
 }
 
-#pragma glslify: export(qinticInOut)
+#pragma glslify: export(quinticInOut)
