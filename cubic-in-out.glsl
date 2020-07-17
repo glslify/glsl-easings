acfac6eefbb91d74b0ce56478322acebd8a02497
Benjamin Schmidt <bmschmidt@gmail.com>
2020-07-17 10:13:01 -0400

    Fix cubic-in-out above .5

--- cubic-in-out.glsl
+++ cubic-in-out.glsl
@@ -1,7 +1,7 @@
 float cubicInOut(float t) {
   return t < 0.5
     ? 4.0 * t * t * t
-    : 0.5 * pow(2.0 * t - 2.0, 3.0) + 1.0;
+    : 1. - 4.0 * pow(1. - t, 3.0);
 }
 
 #pragma glslify: export(cubicInOut)
