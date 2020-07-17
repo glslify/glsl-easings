3f952b529bf6d7424eeda2e2c2bf8a1bb1dfdc2e
Benjamin Schmidt <bmschmidt@gmail.com>
2020-07-17 10:24:41 -0400

    Avoid less than zero to pow call.

--- quartic-in-out.glsl
+++ quartic-in-out.glsl
@@ -1,7 +1,7 @@
 float quarticInOut(float t) {
   return t < 0.5
     ? +8.0 * pow(t, 4.0)
-    : -8.0 * pow(t - 1.0, 4.0) + 1.0;
+    : -8.0 * pow(1.0 - t , 4.0) + 1.0;
 }
 
 #pragma glslify: export(quarticInOut)
